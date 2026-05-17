3520a478964ffdf468e3f833eb8828e33492a1ef
aanjardev <ailapyuu3000@gmail.com>
2026-05-17 23:09:43 +0700

    scene join

--- Assets/Scripts/DoorEvent.cs
+++ Assets/Scripts/DoorEvent.cs
@@ -13,7 +13,7 @@ public class DoorEvent : MonoBehaviour
     void Update()
     {
         // sisa 7 menit
-        if (!triggered && timer.timeRemaining <= 420f)
+        if (!triggered && timer.timeRemaining <= 890f)
         {
             TriggerDoorEvent();
         }
